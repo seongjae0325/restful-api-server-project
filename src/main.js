7f5dcbaef2c26bbf95e14c5651911bfd5a50da21
seongjae0325 <90776802+seongjae0325@users.noreply.github.com>
2022-10-01 18:24:44 +0900

    Add posts list api

--- src/main.js
+++ src/main.js
@@ -24,6 +24,16 @@ const posts = [
         title: 'my first post',
         content: 'Hello!',
     },
+    {
+        id: 'my_second_post',
+        title: 'my second post',
+        content: 'Hello!',
+    },
+    {
+        id: 'my_third_post',
+        title: 'my third post',
+        content: 'Hello!',
+    },
 ];
 
 // Posting
@@ -36,8 +46,15 @@ const server = http.createServer((req, res) => {
     const postIdRegexResult = (req.url && POSTS_ID_REGEX.exec(req.url)) || undefined;
 
     if (req.url === '/posts' && req.method === 'GET') {
+        const result = {
+            totalCount: posts.length,
+            result: posts.map((post) => ({ id: post.id, title: post.title })),
+        };
+
         res.statusCode = 200;
-        res.end('포스트들 리스트 보기');
+        res.setHeader('Content-Type', 'application/json; encoding=utf-8');
+        res.end(JSON.stringify(result));
+        // GET 포스트들 리스트 보기
     } else if (postIdRegexResult) {
         // GET 포스트 보기
         const postId = postIdRegexResult[1];
